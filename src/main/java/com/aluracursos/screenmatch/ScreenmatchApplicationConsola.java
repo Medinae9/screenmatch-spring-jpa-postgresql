e59d6050219847961db1c504955564f86e115396
euc <medina.e9@gmail.com>
2026-01-13 15:42:05 -0600

    conexion a frontend

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java
@@ -0,0 +1,32 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class ScreenmatchApplicationConsola implements CommandLineRunner {
+
+    @Autowired
+    private SerieRepository repository;
+    @Value("${omdb.api.key}")
+    private String apiKey;
+
+    public ScreenmatchApplicationConsola(SerieRepository repository) {
+        this.repository = repository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplicationConsola.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(repository, apiKey);
+        principal.muestraElMenu();
+    }
+}
